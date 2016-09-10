2857b865ea9b1e1cd12099304cafc549693dda25
guerarda <guerarda@users.noreply.github.com>
2016-09-09 22:56:19 -0700

    Visualize normals

--- ray.cpp
+++ ray.cpp
@@ -1,1 +1,6 @@
 #include "ray.hpp"
+
+v3f ray::point_at_param(float t) const
+{
+    return v3f_add(m_org, v3f_smul(t, m_dir));
+}
--- ray.hpp
+++ ray.hpp
@@ -4,7 +4,10 @@
 #include "vec.hpp"
 
 struct ray {
-    ray(const v3f &org, const v3f dir) : m_org(org), m_dir(dir) { }
+        ray(const v3f &org, const v3f dir) : m_org(org), m_dir(dir) { }
+
+    v3f point_at_param(float t) const;
+
 
     v3f m_org;
     v3f m_dir;
