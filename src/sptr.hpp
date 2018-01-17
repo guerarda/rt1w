ce12aff0b84685d0dc079e1271226c5935df0a5b
guerarda <guerarda@users.noreply.github.com>
2018-01-16 23:19:49 -0800

    Formatting

--- src/sptr.hpp
+++ src/sptr.hpp
@@ -11,12 +11,12 @@ using uptr = typename std::unique_ptr<T>;
 
 /* Convenience unique_ptr constructor */
 template<typename T, typename ...Args>
-std::unique_ptr<T> make_unique( Args &&...args )
+std::unique_ptr<T> make_unique(Args &&...args)
 {
     return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 }
 /* Empty struct in order to pass sptr<object> around, without
- *having to template everything. Might be a better way to do this...
+ * having to template everything. Might be a better way to do this...
  */
 struct Object {
     virtual ~Object() { }
