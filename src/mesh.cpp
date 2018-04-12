1f8dcff9e451ae23ca6e217e1df43a0c5bb7b8af
guerarda <guerarda@users.noreply.github.com>
2018-04-11 22:23:20 -0700

    Mesh refactoring
    Add a VertexData struct that can be share between meshes

--- src/mesh.cpp
+++ src/mesh.cpp
@@ -6,51 +6,36 @@
 #include "primitive.hpp"
 #include "utils.h"
 
+#pragma mark - Vertex Data
+
+struct _VertexData : VertexData {
+    _VertexData(size_t nv,
+                uptr<v3f[]> &v,
+                uptr<v3f[]> &n,
+                uptr<v2f[]> &uv) : VertexData(nv, v, n , uv) { }
+};
+
+
+sptr<VertexData> VertexData::create(size_t nv,
+                                    uptr<v3f[]> &v,
+                                    uptr<v3f[]> &n,
+                                    uptr<v2f[]> &uv)
+{
+    return std::make_shared<_VertexData>(nv, v, n, uv);
+}
+
 #pragma mark - Mesh Data
 
 struct MeshData {
 
     MeshData(size_t nt,
-             const sptr<Value> &v,
-             const sptr<Value> &i,
-             const sptr<Value> &n,
-             const sptr<Value> &uv);
+             const sptr<VertexData> &vd,
+             uptr<uint32_t[]> &i) : m_nt(nt), m_vd(vd), m_i(std::move(i)) { }
 
-    size_t m_nt;
-    size_t m_nv;
-    uptr<v3f[]> m_v;
-    uptr<v3f[]> m_n;
-    uptr<v2f[]> m_uv;
+    size_t           m_nt;
+    sptr<VertexData> m_vd;
     uptr<uint32_t[]> m_i;
 };
-
-MeshData::MeshData(size_t nt,
-                   const sptr<Value> &v,
-                   const sptr<Value> &i,
-                   const sptr<Value> &n,
-                   const sptr<Value> &uv)
-{
-    ASSERT(v);
-    ASSERT(i);
-
-    m_nt = nt;
-    m_nv = v->count() / 3;
-
-    m_v = std::make_unique<v3f[]>(m_nv);
-    v->value(TYPE_FLOAT32, m_v.get(), 0, 3 * m_nv);
-
-    m_i = std::make_unique<uint32_t[]>(3 * nt);
-    i->value(TYPE_UINT32, m_i.get(), 0, 3 * nt);
-
-    if (n) {
-        m_n = std::make_unique<v3f[]>(m_nv);
-        n->value(TYPE_FLOAT32, m_n.get(), 0, 3 * m_nv);
-    }
-    if (uv) {
-        m_uv = std::make_unique<v2f[]>(m_nv);
-        uv->value(TYPE_FLOAT32, m_uv.get(), 0, 2 * m_nv);
-    }
-}
 
 #pragma mark - Triangle
 
@@ -59,7 +44,7 @@ struct Triangle : Shape {
     Triangle(const sptr<const MeshData> &md, size_t ix) : m_md(md),
                                                           m_v(&md->m_i[3 * ix]) { }
 
-    bool hit(const sptr<ray> &r, float min, float max, hit_record &rec) const override;
+    bool     hit(const sptr<ray> &, float, float, hit_record &) const override;
     bounds3f bounds() const override;
 
     sptr<const MeshData>   m_md;
@@ -78,10 +63,12 @@ static size_t max_dimension(const v3f &v)
 
 bool Triangle::hit(const sptr<ray> &r, float min, float max, hit_record &rec) const
 {
+    sptr<VertexData> vd = m_md->m_vd;
+
     /* Get triangle coordinates */
-    v3f p0 = m_md->m_v[m_v[0]];
-    v3f p1 = m_md->m_v[m_v[1]];
-    v3f p2 = m_md->m_v[m_v[2]];
+    v3f p0 = vd->m_v[m_v[0]];
+    v3f p1 = vd->m_v[m_v[1]];
+    v3f p2 = vd->m_v[m_v[2]];
 
     /* Transform triangle into ray-space coordinates */
     v3f p0t = p0 - r->origin();
@@ -122,7 +109,7 @@ bool Triangle::hit(const sptr<ray> &r, float min, float max, hit_record &rec) co
     float e2 = (float)((double)p0t.x * (double)p1t.y - (double)p0t.y * (double)p1t.x);
 
     if (   (e0 < 0.0f || e1 < 0.0f || e2 < 0.0f)
-        && (e0 > 0.0f || e1 > 0.0f || e2 > 0.0f)) {
+           && (e0 > 0.0f || e1 > 0.0f || e2 > 0.0f)) {
         return false;
     }
 
@@ -152,10 +139,10 @@ bool Triangle::hit(const sptr<ray> &r, float min, float max, hit_record &rec) co
     v2f uv1 = { 0.0, 1.0 };
     v2f uv2 = { 1.0, 1.0 };
 
-    if (m_md->m_uv) {
-        uv0 = m_md->m_uv[m_v[0]];
-        uv1 = m_md->m_uv[m_v[1]];
-        uv2 = m_md->m_uv[m_v[2]];
+    if (vd->m_uv) {
+        uv0 = vd->m_uv[m_v[0]];
+        uv1 = vd->m_uv[m_v[1]];
+        uv2 = vd->m_uv[m_v[2]];
     }
 
     /* Normals */
@@ -174,9 +161,11 @@ bool Triangle::hit(const sptr<ray> &r, float min, float max, hit_record &rec) co
 
 bounds3f Triangle::bounds() const
 {
-    const v3f &p0 = m_md->m_v[m_v[0]];
-    const v3f &p1 = m_md->m_v[m_v[1]];
-    const v3f &p2 = m_md->m_v[m_v[2]];
+    sptr<VertexData> vd = m_md->m_vd;
+
+    const v3f &p0 = vd->m_v[m_v[0]];
+    const v3f &p1 = vd->m_v[m_v[1]];
+    const v3f &p2 = vd->m_v[m_v[2]];
 
     return Union(bounds3f(p0, p1), p2);
 }
@@ -186,11 +175,13 @@ bounds3f Triangle::bounds() const
 struct _Mesh : Mesh {
     _Mesh(const sptr<MeshData> &md);
 
-    bool hit(const sptr<ray> &r, float min, float max, hit_record &rec) const override;
+    bool     hit(const sptr<ray> &, float, float, hit_record &) const override;
     bounds3f bounds() const override { return m_box; };
 
-    bounds3f m_box;
-    sptr<MeshData> m_md;
+    std::vector<sptr<Shape>> faces() const override;
+
+    bounds3f                    m_box;
+    sptr<MeshData>              m_md;
     std::vector<sptr<Triangle>> m_tris;
 };
 
@@ -216,16 +207,53 @@ bool _Mesh::hit(const sptr<ray> &r, float min, float max, hit_record &rec) const
     return false;
 }
 
+std::vector<sptr<Shape>> _Mesh::faces() const
+{
+    return std::vector<sptr<Shape>>(m_tris.begin(), m_tris.end());
+}
+
 #pragma mark - Static Constructors
 
 sptr<Mesh> Mesh::create(size_t nt,
-                        const sptr<Value> &v,
-                        const sptr<Value> &i,
-                        const sptr<Value> &n,
-                        const sptr<Value> &uv)
+                        const sptr<VertexData> &vd,
+                        const sptr<Value> &indices)
 {
-    sptr<MeshData> md = std::make_shared<MeshData>(nt, v, i, n, uv);
+    uptr<uint32_t[]> i = std::make_unique<uint32_t[]>(3 * nt);
+    indices->value(TYPE_UINT32, i.get(), 0, 3 * nt);
+
+    sptr<MeshData> md = std::make_shared<MeshData>(nt, vd, i);
+
     return std::make_shared<_Mesh>(md);
+}
+
+sptr<Mesh> Mesh::create(size_t nt,
+                        const sptr<Value> &vertices,
+                        const sptr<Value> &indices,
+                        const sptr<Value> &normals,
+                        const sptr<Value> &uvs)
+{
+    ASSERT(vertices);
+    ASSERT(indices);
+
+    size_t nv = vertices->count() / 3;
+
+    uptr<v3f[]> v = std::make_unique<v3f[]>(nv);
+    vertices->value(TYPE_FLOAT32, v.get(), 0, 3 * nv);
+
+    uptr<v3f[]> n;
+    if (normals) {
+        n = std::make_unique<v3f[]>(3 * nv);
+        normals->value(TYPE_FLOAT32, n.get(), 0, 3 * nv);
+    }
+    uptr<v2f[]> uv;
+    if (uv) {
+        uv = std::make_unique<v2f[]>(2 * nv);
+        uvs->value(TYPE_FLOAT32, uv.get(), 0, 2 * nv);
+    }
+
+    sptr<VertexData> vd = VertexData::create(nv, v, n, uv);
+
+    return Mesh::create(nt, vd, indices);
 }
 
 sptr<Mesh> Mesh::create(const sptr<Params> &p)
@@ -243,8 +271,8 @@ sptr<Mesh> Mesh::create(const sptr<Params> &p)
         bool ok_uv = uv ? 2 * v->count() == 3 * uv->count() : true;
 
         if (ok_i && ok_n && ok_uv) {
-             return Mesh::create(nt, v, i, n, uv);
-         }
+            return Mesh::create(nt, v, i, n, uv);
+        }
         WARNING_IF(!ok_i,  "Triangle count doesn't match index count");
         WARNING_IF(!ok_n,  "Normal count doesn't match vertex count");
         WARNING_IF(!ok_uv, "UV count doesn't match vertex count");
--- src/mesh.hpp
+++ src/mesh.hpp
@@ -7,19 +7,39 @@
 struct Params;
 struct Value;
 
+struct VertexData : Object {
+
+    static sptr<VertexData> create(size_t nv,
+                                   uptr<v3f[]> &v,
+                                   uptr<v3f[]> &n,
+                                   uptr<v2f[]> &uv);
+    const size_t            m_nv;
+    const uptr<const v3f[]> m_v;
+    const uptr<const v3f[]> m_n;
+    const uptr<const v2f[]> m_uv;
+
+protected:
+    VertexData(size_t nv,
+               uptr<v3f[]> &v,
+               uptr<v3f[]> &n,
+               uptr<v2f[]> &uv) : m_nv(nv),
+                                  m_v(std::move(v)),
+                                  m_n(std::move(n)),
+                                  m_uv(std::move(uv)) { }
+};
+
 struct Mesh : Shape {
+    static sptr<Mesh> create(size_t nt,
+                             const sptr<Value> &vertices,
+                             const sptr<Value> &indices,
+                             const sptr<Value> &normals,
+                             const sptr<Value> &uvs);
 
     static sptr<Mesh> create(size_t nt,
-                             const sptr<Value> &v,
-                             const sptr<Value> &i,
-                             const sptr<Value> &n,
-                             const sptr<Value> &uv);
+                             const sptr<VertexData> &vd,
+                             const sptr<Value> &indices);
 
     static sptr<Mesh> create(const sptr<Params> &params);
 
-    virtual bool     hit(const sptr<ray> &r,
-                         float min,
-                         float max,
-                         hit_record &rec) const = 0;
-    virtual bounds3f bounds() const = 0;
+    virtual std::vector<sptr<Shape>> faces() const = 0;
 };
