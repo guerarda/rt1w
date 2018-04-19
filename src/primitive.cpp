7ba2e541ee7bef646d90e379fe64fb85211d75b1
guerarda <guerarda@users.noreply.github.com>
2018-04-18 22:32:52 -0700

    Add Aggregate class for grouping primitives

--- src/primitive.cpp
+++ src/primitive.cpp
@@ -30,7 +30,7 @@ bounds3f _Primitive::bounds() const
     return m_shape->bounds();
 }
 
-#pragma mark - Static constructors
+#pragma mark - Static constructor;
 
 sptr<Primitive> Primitive::create(const sptr<Shape> &s, const sptr<Material> &m)
 {
@@ -41,4 +41,51 @@ sptr<Primitive> Primitive::create(const sptr<Shape> &s, const sptr<Material> &m)
     WARNING_IF(!m, "Primitive has no material");
 
     return nullptr;
+}
+
+#pragma mark - Aggregate
+
+struct _Aggregate : Aggregate {
+
+    _Aggregate(const std::vector<sptr<Primitive>> &prims);
+
+    bool     hit(const sptr<ray> &, float, float, hit_record &) const override;
+    bounds3f bounds() const override { return m_bounds; }
+
+    const std::vector<sptr<Primitive>> &primitives() const override { return m_primitives; }
+
+    std::vector<sptr<Primitive>> m_primitives;
+    bounds3f m_bounds;
+};
+
+_Aggregate::_Aggregate(const std::vector<sptr<Primitive>> &prims)
+{
+    m_primitives = prims;
+    for (auto &p : m_primitives) {
+        m_bounds = Union(m_bounds, p->bounds());
+    }
+}
+
+bool _Aggregate::hit(const sptr<ray> &r, float min, float max, hit_record &rec) const
+{
+    bool hit = false;
+    rec.t = std::numeric_limits<float>::max();
+
+    for (auto &p : m_primitives) {
+        hit_record hr;
+        if (p->hit(r, min, max, hr)) {
+            if (hr.t < rec.t) {
+                rec = hr;
+            }
+            hit = true;
+        }
+    }
+    return hit;
+}
+
+#pragma mark - Static Constructor
+
+sptr<Aggregate> Aggregate::create(const std::vector<sptr<Primitive>> primitives)
+{
+    return std::make_shared<_Aggregate>(primitives);
 }
--- src/primitive.hpp
+++ src/primitive.hpp
@@ -3,6 +3,8 @@
 #include "ray.hpp"
 #include "types.h"
 
+#include <vector>
+
 struct Material;
 struct Shape;
 
@@ -15,6 +17,7 @@ struct hit_record {
 };
 
 struct Primitive : Object {
+
     static sptr<Primitive> create(const sptr<Shape> &s,
                                   const sptr<Material> &m);
     static sptr<Primitive> load_obj(const std::string &path);
@@ -25,3 +28,10 @@ struct Primitive : Object {
                           hit_record &rec) const = 0;
     virtual bounds3f  bounds() const = 0;
  };
+
+struct Aggregate : Primitive {
+
+    static sptr<Aggregate> create(const std::vector<sptr<Primitive>> prims);
+
+    virtual const std::vector<sptr<Primitive>> &primitives() const = 0;
+};
