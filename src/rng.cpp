4a6e6b153501077bb2cc95c2b86fefd911c46e10
guerarda <guerarda@users.noreply.github.com>
2018-09-23 12:39:48 -0700

    Faster PRNG

--- src/rng.cpp
+++ src/rng.cpp
@@ -5,9 +5,53 @@
 
 #include "error.h"
 
+/* See http://xoshiro.di.unimi.it */
+static inline uint64_t splitmix64(uint64_t x)
+{
+    uint64_t z = (x += 0x9e3779b97f4a7c15);
+    z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+    z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+    return z ^ (z >> 31);
+}
+
+static inline uint64_t rotl(const uint64_t x, int k) {
+    return (x << k) | (x >> (64 - k));
+}
+
+struct xoroshiro256plus {
+    xoroshiro256plus(uint64_t seed) {
+        uint64_t x = seed;
+        for (size_t i = 0; i < 4; i++) {
+            x = splitmix64(x);
+            s[i] = x;
+        }
+    }
+    uint64_t next() {
+        const uint64_t r = s[0] + s[3];
+        const uint64_t t = s[1] << 17;
+
+        s[2] ^= s[0];
+        s[3] ^= s[1];
+        s[1] ^= s[2];
+        s[0] ^= s[3];
+
+        s[2] ^= t;
+        s[3] = rotl(s[3], 45);
+
+        return r;
+    }
+
+    double uniform_double() {
+        uint64_t x = next();
+        return (x >> 11) * (1. / (UINT64_C(1) << 53));
+    }
+
+    uint64_t s[4];
+};
+
 struct _RNG : RNG {
 
-    void init();
+    _RNG(uint64_t seed) : m_xs256p(xoroshiro256plus(seed)) { }
 
     uint32_t u32() override;
     float    f32() override;
@@ -15,16 +59,8 @@ struct _RNG : RNG {
     uint32_t u32(uint32_t b) override;
     float    f32(float b) override;
 
-    std::mt19937 m_mt;
-    std::uniform_real_distribution<float> m_dist;
+    xoroshiro256plus m_xs256p;
 };
-
-
-void _RNG::init()
-{
-    m_mt = std::mt19937(std::random_device()());
-    m_dist = std::uniform_real_distribution<float>(0.0, 1.0);
-}
 
 uint32_t _RNG::u32()
 {
@@ -36,7 +72,7 @@ float _RNG::f32()
 {
     float f;
     do {
-        f = m_dist(m_mt);
+        f = (float)m_xs256p.uniform_double();
     } while (f >= 1.0f);
     return f;
 }
@@ -60,7 +96,5 @@ float _RNG::f32(float b)
 
 sptr<RNG> RNG::create()
 {
-    auto rng = std::make_shared<_RNG>();
-    rng->init();
-    return rng;
+    return std::make_shared<_RNG>(std::random_device()());
 }
