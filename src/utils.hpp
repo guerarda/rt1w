24bea1745f823f34269432bd5f1058281ac3a572
guerarda <guerarda@users.noreply.github.com>
2019-01-16 00:08:50 -0800

    Add Floating point utils

--- src/utils.hpp
+++ src/utils.hpp
@@ -15,7 +15,7 @@ constexpr double InvPi = 0.318309886183790671537767526745028724;
 constexpr double Inv2Pi = 0.636619772367581343075535053490057448;
 constexpr double Sqrt2 = 1.41421356237309504880168872420969808;
 
-#pragma mark - Floating-point comparisons
+#pragma mark - Floating Point comparisons
 
 template <typename T>
 inline int32_t FloatCompare(T a, T b)
@@ -33,6 +33,51 @@ template <typename T>
 inline bool FloatEqual(T a, T b)
 {
     return FloatCompare(a, b) == 0;
+}
+
+#pragma mark - Floating Point Misc
+inline uint32_t FloatToBits(float f)
+{
+    uint32_t ui;
+    memcpy(&ui, &f, sizeof(f));
+    return ui;
+}
+
+inline float BitsToFloat(uint32_t ui)
+{
+    float f;
+    memcpy(&f, &ui, sizeof(ui));
+    return f;
+}
+
+inline uint64_t FloatToBits(double f)
+{
+    uint64_t ui;
+    memcpy(&ui, &f, sizeof(f));
+    return ui;
+}
+
+inline double BitsToFloat(uint64_t ui)
+{
+    double f;
+    memcpy(&f, &ui, sizeof(ui));
+    return f;
+}
+
+template <typename T>
+inline T NextFloatUp(T f)
+{
+    static_assert(std::is_floating_point<T>::value,
+                  "Floating Point function with non-floating point argument");
+    return std::nexttoward(f, std::numeric_limits<T>::infinity());
+}
+
+template <typename T>
+inline T NextFloatDown(T f)
+{
+    static_assert(std::is_floating_point<T>::value,
+                  "Floating Point function with non-floating point argument");
+    return std::nexttoward(f, -std::numeric_limits<T>::infinity());
 }
 
 #pragma mark - Trigonometric functions
