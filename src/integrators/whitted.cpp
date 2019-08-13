c1524e4b648107559432bc3b77bac2bc1a6f303f
guerarda <guerarda@users.noreply.github.com>
2019-08-13 10:24:35 -0700

    Fix Whitted Integrator

--- src/integrators/whitted.cpp
+++ src/integrators/whitted.cpp
@@ -1,5 +1,6 @@
 #include "integrators/whitted.hpp"
 
+#include "rt1w/bxdf.hpp"
 #include "rt1w/interaction.hpp"
 #include "rt1w/light.hpp"
 #include "rt1w/material.hpp"
@@ -20,7 +21,6 @@ struct _WhittedIntegrator : WhittedIntegrator {
 
     sptr<Sampler> m_sampler;
     size_t m_maxDepth;
-    Spectrum m_background;
 };
 
 Spectrum _WhittedIntegrator::Li(const Ray &ray,
@@ -31,33 +31,46 @@ Spectrum _WhittedIntegrator::Li(const Ray &ray,
                                 Spectrum *A) const
 {
     Interaction isect;
-    if (scene->intersect(ray, isect)) {
-        Spectrum attenuation;
+    if (!scene->intersect(ray, isect)) {
+        Spectrum L;
+        for (const auto &light : scene->lights()) {
+            L += light->Le(ray);
+        }
+        return L;
+    }
+    sptr<BSDF> bsdf = ComputeBSDF(isect);
+    Spectrum L = LightEmitted(isect, isect.wo);
+
+    for (const auto &light : scene->lights()) {
         v3f wi;
-        if (depth < m_maxDepth && isect.mat->scatter(ray, isect, attenuation, wi)) {
-            Spectrum L;
-            for (const auto &light : scene->lights()) {
-                v3f lwi;
-                VisibilityTester vis;
-                v2f u = sampler->sample2D();
-                float pdf;
-                Spectrum Li = light->sample_Li(isect, u, lwi, pdf, vis);
-                if (vis.visible(scene)) {
-                    L += Li * isect.mat->f(isect, isect.wo, lwi);
-                }
-            }
-            Ray scattered = SpawnRay(isect, wi);
-            L += attenuation * Li(scattered, scene, sampler, depth + 1);
-            if (N) {
-                *N = isect.n;
-            }
-            if (A) {
-                *A = attenuation;
-            }
-            return L;
+        VisibilityTester vis;
+        v2f u = sampler->sample2D();
+        float pdf;
+        Spectrum Li = light->sample_Li(isect, u, wi, pdf, vis);
+        if (Li.isBlack() || FloatEqual(pdf, .0f)) {
+            continue;
+        }
+        Spectrum f = bsdf->f(isect.wo, wi);
+
+        if (!f.isBlack() && vis.visible(scene)) {
+            L += Li * f * AbsDot(wi, isect.shading.n) / pdf;
         }
     }
-    return m_background;
+    v3f wi;
+    float pdf;
+    Spectrum f = bsdf->sample_f(isect.wo, sampler->sample2D(), wi, pdf);
+    if (depth + 1 < m_maxDepth) {
+        L += f * Li(SpawnRay(isect, wi), scene, sampler, depth + 1)
+             * AbsDot(wi, isect.shading.n) / pdf;
+    }
+
+    if (N) {
+        *N = isect.n;
+    }
+    if (A) {
+        *A = f;
+    }
+    return L;
 }
 
 #pragma mark - Static constructor
